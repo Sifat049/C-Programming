d077d6bde666cd2abc80bc7fa57520d8458231ff
Md.MishkaT_uL Bary Sifat <144310771+Sifat049@users.noreply.github.com>
2025-02-25 01:11:41 +0600

    Add files via upload

---
+++ for loop/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  while (1)
+  {
+      int i;
+   i=1;
+   /*for (i=-1; i<=10 ;  ++i)
+  {
+      printf("                %d\n",i);
+    printf("     C PROGRAM           %d\n");
+
+
+  }
+
+  while(i<=10)
+  {
+      printf("                              %d\n",i);
+      i++;
+  }*/
+  do {
+    printf("               %d \n",i);
+       printf("      SIFAT      ");
+    i++;
+  }while(i<=10);
+  }
+    return 0;
+}
