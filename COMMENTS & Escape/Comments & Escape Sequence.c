4fc51880f84fdf05658334ef0bc071ca2cf75a45
Md.MishkaT_uL Bary Sifat <144310771+Sifat049@users.noreply.github.com>
2025-02-25 01:10:59 +0600

    Add files via upload

---
+++ COMMENTS & Escape/Comments & Escape Sequence.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+
+{
+    printf( "Single line comments  example=//\n\n" );                  // single line comments
+
+
+    printf("double line comments example=/*..............*/\n\n");                             /* double line comments
+                                                                                                           multiples line
+                                                                                                            uses*/
+
+   printf("Escape sequence example=\'   \"   \a  \b   \f      \t   \v   \\    \ooo \n\n\n");
+   /*           escape sequence                 meaning
+                    \a                                     alert bell
+                    \b                                     backspace
+                    \f                                      form feed
+                    \n                                     new line
+                    \r                                      carrige return
+                    \t                                      horigontal tab
+                    \v                                      vertical tab
+                    \0                                     Null character
+                    \'                                      single quetation
+                    \"                                      qouble quote
+                    \\                                     backslash
+                    \?                                      questionmark
+                    \ooo                               octal number
+                     \xhhh                            hexadecimel number
+*/
+
+
+
+
+    printf("Welcome !  The wonderful world......\n");
+    return 0;
+}
+
