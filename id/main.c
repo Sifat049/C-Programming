43e7143d37e54b8a9ac8efdc2f87029012de8296
Md.MishkaT_uL Bary Sifat <144310771+Sifat049@users.noreply.github.com>
2025-02-25 01:12:32 +0600

    Add files via upload

---
+++ id/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+char a,b,c,d,e,f,g,h,i,j;
+printf("Enter Your ID : ");
+
+ //int a,b,c,d,e,f,g,h,i,j;
+  //  scanf("%d%d%d%d%d%d%d%d%d%d",&a,&b,&c,&d,&e,&f,&g,&h,&i,&j);
+    scanf("%c%c%c%c%c%c%c%c%c%c",&a,&b,&c,&d,&e,&f,&g,&h,&i,&j);
+  //  printf("%d%d%d batch  \n",d,e,f);
+        printf("%c%c%c batch  \n",d,e,f);
+
+    return 0;
+}
