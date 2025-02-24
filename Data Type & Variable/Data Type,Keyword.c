d8e5dccb3048e88762d905da787d868722fb5c82
Md.MishkaT_uL Bary Sifat <144310771+Sifat049@users.noreply.github.com>
2025-02-25 01:11:04 +0600

    Add files via upload

---
+++ Data Type & Variable/Data Type,Keyword.c
@@ -0,0 +1,21 @@
+//32  keywords:  keyword always small letter.majhe kono faka  thake na
+
+//কখনো যদি দটি  keyword use হয় ,তবে ফাঁকা স্থান থাকে ।।
+
+#include<stdio.h>
+int main()
+{
+    /*  int num;
+      float b;
+      char c;
+      double lf;
+
+        do    ;break  ; volatile    ;  while  ;
+        if    ;default     ; extern     ;  unsigned      ; for ;
+        sizeof  ; short  ; union  ; enum    ;  goto  ;
+        switch    ;const  ; case      ;else  ; register  ;
+        long ; static ;return ;short; struct;     */
+
+        printf("keyword ");
+        return 0;
+}
