4115083f6fbc5af6b1e0a193c25c66cd1528625a
Md.MishkaT_uL Bary Sifat <144310771+Sifat049@users.noreply.github.com>
2025-02-25 01:08:45 +0600

    Add files via upload

---
+++ ASCII Value/ASCII Value.c
@@ -0,0 +1,25 @@
+#include <stdio.h> /*              A=65   B=66 Z=90
+                                                   jkono word choto boror modhe parthokko 32
+                                                   a=65+32 =97
+
+                                                                */
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+   int n;
+   char a;
+    printf("enter any number:");
+    scanf("%d %c",&n,&a);
+    printf("Ascii value: %c %d\n",n,a);
+
+
+    return 0;
+}
+
+
+
+
+
+
